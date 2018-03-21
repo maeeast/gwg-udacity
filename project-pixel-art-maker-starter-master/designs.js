7d8f5cd77caf4c5c627b99b3e54b8e91c55d63e6
Jenn Maher <maeeast@me.com>
2018-03-21 09:56:10 -0400

    Pixel Art Project

--- project-pixel-art-maker-starter-master/designs.js
+++ project-pixel-art-maker-starter-master/designs.js
@@ -1,71 +1,60 @@
 // Select color input
-var storedColor, rows, cols,gridContainer;
+var rows, cols, storedColor;
 
 function getPixelColor() {
-  storedColor = document.getElementById("colorPicker");
+  var storedColor = $('#colorPicker').val();
+  //storedColor = document.getElementById('colorPicker').value;
+  console.log(storedColor);
 }
 
 // Select size input
 function getTableSize() {
-  rows = document.getElementById("inputHeight");
-  cols = document.getElementById("inputWidth");
+  rows = $('#inputHeight').val();
+  cols = $('#inputWidth').val();
+  //rows = document.getElementById('inputHeight').value;
+  //cols = document.getElementById('inputWidth').value;
+  console.log(rows);
+  console.log(cols);
 }
-
-function createTable() {
-  gridContainer = document.getElementById("pixelCanvas");
-  if (!gridContainer) {
-    // throw error
-    console.error("Problem: no div for the grid table!");
-  }
-  var table = document.createElement("table");
-  table.setAttribute("id", "canvas")
-
-  for (var i = 0; i < rows; i++) {
-      var tr = document.createElement("tr");
-      for (var j = 0; j < cols; j++) {
-          var cell = document.createElement("td");
-          cell.setAttribute("id", i + "_" + j);
-          cell.setAttribute("class", "off");
-          cell.setAttribute("class", "cell");
-          cell.onclick = cellClickHandler;
-          tr.appendChild(cell);
-      }
-      table.appendChild(tr);
-  }
-  gridContainer.appendChild(table);
-}
-
 
 // When size is submitted by the user, call makeGrid()
 
+function makeGrid() {
+  $('#pixelCanvas').children().remove();
+  getTableSize();
+  getPixelColor();
+  //var table = $('#pixelCanvas');
+  var table = document.getElementById('pixelCanvas');
+  console.log(table);
+  for (var i = 0; i < rows; i++) {
+      var tr = table.insertRow(i);
+      for (var j = 0; j < cols; j++) {
+          var cell = tr.insertCell(j);
+          cell.setAttribute("id", i + "_" + j);
+          cell.setAttribute("class", "off");
+          cell.setAttribute("class", "cell");
+          tr.appendChild(cell);
+      }
+  }
+}
+
+function colorCell() {
+  $(this).css('background-color', $('#colorPicker').val());
+}
 
 // initialize
 function initialize() {
     getPixelColor();
-    createTable();
+    makeGrid();
     getTableSize();
 }
-console.log(storedColor);
-console.log(rows);
-console.log(cols);
-// lay out the board
-
-
-
-function cellClickHandler() {
-    var classes = this.getAttribute("class");
-    if (classes.indexOf("on") > -1) {
-        this.setAttribute("class", "off");
-    } else {
-        this.setAttribute("class", "on");
-        var storedColor = getPixelColor();
-        this.style.backgroundColor = storedColor;
-
-
-    }
-}
-
-
 // start everything
 
 window.onload = initialize;
+
+$(":submit").click(function(){
+    event.preventDefault();
+    makeGrid();
+});
+
+$('#pixelCanvas').on('click', 'td', colorCell);
